593dab4b8bdfc1e5cb3490cc8911aa9074d3c654
Kyle Maglietta <kmaglietta2013@fau.edu>
2017-04-11 17:39:27 -0400

    Angular implemenation

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-function cllCallAPI () {
-  $.ajax({
-    url:'functions.php',
-    complete: function (response) {
-      console.log(response.responseText);
-      var json = JSON.parse(response.responseText);
-      return json;
-    },
-    error: function () {
-      console.log("error occured obviously ...");
-      return null;
-    }
-  });
-}
+var randAPIApp = angular.module('randAPIApp', []);
+
+randAPIApp.controller('RandAPIController', function
+RandAPIController ($scope) {
+  $scope.getQuote = function () {
+    $.ajax({
+      url:'functions.php',
+      complete: function (response) {
+        var json = JSON.parse(response.responseText);
+        console.log(json);
+        $scope.author = json.author;
+        $scope.body = json.body;
+        $scope.$apply();
+      },
+      error: function () {
+        console.log("error occured obviously ...");
+      }
+    });
+  }
+});
