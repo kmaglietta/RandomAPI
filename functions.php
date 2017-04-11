fcaa2585db4587e50fd0809e7d0ca41d6106aaf8
Kyle Maglietta <kmaglietta2013@fau.edu>
2017-04-11 15:33:17 -0400

    Seperated indep.php -> index.html & functions.php

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+function callAPI($method, $url, $data = false) {
+  $curl = curl_init();
+
+  switch ($method) {
+    case 'POST':
+      echo "post how";
+      curl_setopt($curl, CURLOPT_POST, 1);
+      break;
+
+    default:
+      # code...
+      break;
+  }
+
+  curl_setopt($curl, CURLOPT_URL, $url);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+  $result = curl_exec($curl);
+
+  curl_close($curl);
+
+  var_dump ($result);
+}
+
+callAPI('POST','https://986q6ottq1.execute-api.us-west-2.amazonaws.com/prod/getquote');
+?>
