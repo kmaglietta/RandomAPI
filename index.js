a7501b6a5d3588174b52fb446bb6f9e80798daae
Kyle Maglietta <kmaglietta2013@fau.edu>
2017-04-11 16:45:38 -0400

    Everything is working

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ function cllCallAPI () {
     url:'functions.php',
     complete: function (response) {
       console.log(response.responseText);
+      var json = JSON.parse(response.responseText);
+      return json;
     },
     error: function () {
       console.log("error occured obviously ...");
+      return null;
     }
   });
 }
