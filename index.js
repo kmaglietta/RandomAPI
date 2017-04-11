148d504a4dcabd6990cef15a530ba89609cb8db0
Kyle Maglietta <kmaglietta2013@fau.edu>
2017-04-11 17:35:36 -0400

    Trie to shortcut but need to rethink things

--- index.js
+++ index.js
@@ -1,26 +1,5 @@
 var randAPI = angular.module('randApi',['ngRoute']);
 
-randAPI.controller('MainControler', function MainControler($scope, $http){
-  console.log("Sending to Microsoft ..." );
+randAPI.controller('MainControler', function MainControler($scope){
 
-  var apiURL = "https://986q6ottq1.execute-api.us-west-2.amazonaws.com/prod/getquote";
-
-  var config = {
-    headers : {
-      "Access-Control-Allow-Origin" : "*"
-    }
-  }
-
-  $http.post(apiURL)                    //send request
-  .success(function (data) {                          //200 response
-    if (data[0]) {                                    //face detected and retrive the data
-      console.log(data);
-    }
-    else {
-
-    }
-  })
-  .error(function (data){                             //400 error
-    console.log("error");
-  });
 });
